cefd2e297a628067c8325366981eb69fd1ad5dc7
sudha3295 <41017134+sudha3295@users.noreply.github.com>
2024-03-17 05:01:38 +0000

    tryingout things

--- Hello.py
+++ Hello.py
@@ -21,10 +21,10 @@ LOGGER = get_logger(__name__)
 def run():
     st.set_page_config(
         page_title="Hello",
-        page_icon="👋",
+        page_icon="❤️",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    st.write("# Welcome to Streamlit! 🎶")
 
     st.sidebar.success("Select a demo above.")
 
